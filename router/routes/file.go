ac6db3a7e4292ee88520b2fb5e13b0bf62dd4572
Alistair Smith <alistairsmith01@gmail.com>
2021-06-23 23:08:12 +0100

    a

--- router/routes/file.go
+++ router/routes/file.go
@@ -14,29 +14,37 @@ func File(hostUrl string) structs.Route {
 		Name:    "/file",
 		HostUrl: hostUrl,
 	}
+
 	return structs.Route{
 		Endpoint: point,
 		Callback: func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Add("Access-Control-Allow-Origin", "*")
 			query := r.URL.Query()
 			fileName, present := query["name"]
+
 			if !present || len(fileName) != 1 {
 				w.Header().Add("content-type", "application/json")
 				_, _ = w.Write([]byte("{\"error\": \"invalid arguments\"}"))
 				w.WriteHeader(400)
 				return
 			}
+
 			filePath := "./content/" + fileName[0]
+
 			if doesFileExist(filePath) {
 				file, _ := os.Open(filePath)
 				bz := bytes.NewBuffer(nil)
 				_, _ = io.Copy(bz, file)
+
 				contentType := http.DetectContentType(bz.Bytes())
+
 				w.Header().Add("content-type", contentType)
 				w.WriteHeader(200)
 				_, _ = w.Write(bz.Bytes())
+
 				return
 			}
+
 			w.WriteHeader(404)
 		},
 	}
@@ -44,8 +52,10 @@ func File(hostUrl string) structs.Route {
 
 func doesFileExist(file string) bool {
 	_, err := os.Stat(file)
+
 	if err != nil {
 		return false
 	}
+
 	return true
 }
